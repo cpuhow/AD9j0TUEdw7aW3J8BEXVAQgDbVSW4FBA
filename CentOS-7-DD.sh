53a0d1a5becd1870843177aa62b931e65a5c85b2
cpuhow <67900373+cpuhow@users.noreply.github.com>
2020-07-07 14:11:09 +0800

    Delete CentOS-7-DD.sh

--- CentOS-7-DD.sh
+++
@@ -1,2 +0,0 @@
-yum -y install wget
-wget --no-check-certificate -O-  'https://www.googleapis.com/drive/v3/files/1jnNY7E8oH9CQmkbSCh4kdWElVWCuTwmL?alt=media&key=AIzaSyAMgpypCNDCQM_JmJRWfGwCmdl4yDANaJU' | gunzip | dd of=/dev/sda
